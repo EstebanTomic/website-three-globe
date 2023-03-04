187b2e86750ab149a452be22647b5dfdaeb8be8c
EstebanTomic <e.tomic@gmail.com>
2023-03-04 17:49:32 -0300

    feature: Added animation

--- main.js
+++ main.js
@@ -34,23 +34,116 @@ const camera = new THREE.PerspectiveCamera(
     75,
     innerWidth / innerHeight,
     0.1,
-    1000
+    100000
 )
+camera.position.z = 1;
+camera.rotation.x = 1.16;
+camera.rotation.y = -0.12;
+camera.rotation.z = 0.27;
+//camera.position.z = 15
 
 
-
+// Render
 const renderer = new THREE.WebGLRenderer(
     {
         antialias: true
     }
 )
-renderer.setSize(innerWidth, innerHeight)
-renderer.setPixelRatio(window.devicePixelRatio)
+//scene.fog = new THREE.FogExp2(0x00111f, 0.002);
+//renderer.setClearColor(scene.fog.color);
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
 document.body.appendChild(renderer.domElement)
 
+
+// Lights
+let cloudParticles = [], flash, rain, rainGeo, rainCount = 25000, cloud;
+const ambient = new THREE.AmbientLight(0x555555);
+scene.add(ambient);
+
+const directionalLight = new THREE.DirectionalLight(0xffeedd);
+directionalLight.position.set(0,0,1);
+scene.add(directionalLight);
+
+flash = new THREE.PointLight(0x062d89, 30, 500 ,1.7);
+flash.position.set(200,300,100);
+scene.add(flash);
+
+//Rain
+const vertex = new THREE.Vector3();
+rainGeo = new THREE.BufferGeometry();
+const vertices = [];
+for (let i = 0; i < rainCount; i++) {
+    vertices.push(
+        Math.random() * 520 - 160,
+        Math.random() * 1500 - 150,
+        Math.random() * 230 - 160
+    );
+}
+rainGeo.setAttribute( 'position', new THREE.Float32BufferAttribute( vertices, 3 ) );
+const rainMaterial = new THREE.PointsMaterial({
+    color: 0xaaaaaa,
+    size: 0.1,
+    transparent: true
+});
+rain = new THREE.Points(rainGeo,rainMaterial);
+console.log(rain);
+scene.add(rain);
+
+// Nube
+let loader = new THREE.TextureLoader();
+loader.load("./img/smoke.png", function(texture){
+    const cloudGeo = new THREE.PlaneBufferGeometry(500,500);
+    const cloudMaterial = new THREE.MeshLambertMaterial({
+        map: texture,
+        transparent: true
+    });
+
+    for(let p=0; p<40; p++) {
+        cloud = new THREE.Mesh(cloudGeo,cloudMaterial);
+        cloud.position.set(
+            Math.random()*800 - 400,
+            200,
+            Math.random()*700 - 450
+        );
+        cloud.rotation.x = 1.16;
+        cloud.rotation.y = -0.12;
+        cloud.rotation.z = Math.random()*360;
+        cloud.material.opacity = 0.6;
+        cloudParticles.push(cloud);
+        scene.add(cloud);
+    }
+    animate();
+});
+
+function rainVariation() {
+    let positionAttribute = rain.geometry.getAttribute( 'position' );
+    for ( let i = 0; i < positionAttribute.count; i ++ ) {
+        vertex.fromBufferAttribute( positionAttribute, i );
+        vertex.y -= 1;
+        if (vertex.y < - 60) {
+            vertex.y = 90;
+        }
+        positionAttribute.setXYZ( i, vertex.x, vertex.y, vertex.z );
+    }
+    positionAttribute.needsUpdate = true;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// -- EARTH --
 // Sphere
 const sphere = new THREE.Mesh(
-    new THREE.SphereGeometry(50, 50, 50),
+    new THREE.SphereGeometry(600, 50, 50),
     new THREE.ShaderMaterial({
         vertexShader,
         fragmentShader,
@@ -64,7 +157,7 @@ const sphere = new THREE.Mesh(
 
 // Atmosphere
 const atmosphere = new THREE.Mesh(
-    new THREE.SphereGeometry(54, 50, 50),
+    new THREE.SphereGeometry(600, 50, 50),
     new THREE.ShaderMaterial({
         vertexShader: atmosphereVertexShader,
         fragmentShader: atmosphereFragmentShader,
@@ -73,14 +166,13 @@ const atmosphere = new THREE.Mesh(
     })
 )
 atmosphere.scale.set(1.1, 1.1, 1.1)
-camera.position.z = 15
 
 // Stars
 const starVertices = []
-for (let i = 0; i < 10000; i++) {
-    const x = (Math.random() - 0.5) * 2000
-    const y = (Math.random() - 0.5) * 2000
-    const z = -Math.random() * 2000
+for (let i = 0; i < 20000; i++) {
+    const x = (Math.random() - 0.5) * 60000
+    const y = (Math.random() - 0.5) * 60000
+    const z = -Math.random() * 60000
     starVertices.push(x, y, z)
 }
 const starGeometry = new THREE.BufferGeometry()
@@ -94,7 +186,7 @@ const stars = new THREE.Points(starGeometry, starMaterial)
 function convertLatLngToCartesian(p) {
     let lat = (90 - p.lat) * (Math.PI/180);
     let lng = (p.lng +180) * (Math.PI/180);
-    let radius = 50;
+    let radius = 600;
 
     let x = -(radius * Math.sin(lat)*Math.cos(lng));
     let y = (radius * Math.sin(lat)*Math.sin(lng));
@@ -142,12 +234,12 @@ group.add(stars)
 scene.add(group)
 
 // Controls
-const controls = new OrbitControls( camera, renderer.domElement );
-// controls.maxPolarAngle = Math.PI * 0.495;
-// controls.target.set( 0, 10, 0 );
-controls.minDistance = 2.0;
-controls.maxDistance = 2000.0;
-controls.update();
+//const controls = new OrbitControls( camera, renderer.domElement );
+//// controls.maxPolarAngle = Math.PI * 0.495;
+//// controls.target.set( 0, 10, 0 );
+//controls.minDistance = 2.0;
+//controls.maxDistance = 2000.0;
+//controls.update();
 
 const mouse = {
     x: undefined,
@@ -155,13 +247,38 @@ const mouse = {
 }
 
 function animate() {
+    // Animacion LLuvia
+    rainVariation()
+    cloudParticles.forEach(p => {
+        p.rotation.z -=0.001;
+    });
+    rain.rotation.y +=0.002;
+    if(Math.random() > 0.95 || flash.power > 100) {
+        if(flash.power < 100)
+            flash.position.set(
+                Math.random()*400,
+                300 + Math.random() *200,
+                100
+            );
+        flash.power = 50 + Math.random() * 500;
+    }
+
     requestAnimationFrame(animate)
     renderer.render(scene, camera)
-  gsap.to(group.rotation,{
-      x: -mouse.y * 0.3,
-      y: mouse.x * 0.5,
-      duration: 1.5
-  })
+
+    const timeline = new gsap.timeline();
+    timeline.from(camera.position,
+        { y: -20, duration: 8 },
+        "<")
+        .to(camera.position,{
+            z: 1500,
+            duration: 0.5
+        },'start')
+        .to(camera.rotation,{
+            x: -0.1,
+            duration: 3
+        },'start');
+
 }
 
 animate()
